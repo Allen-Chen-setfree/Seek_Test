c2d58eedde10154282d14a3a19493ff855571408
unknown <allen.setfree.chen@gmail.com>
2018-02-25 23:07:35 +1100

    included poi-ooxml in POM

--- testSeek/src/main/java/au/com/seek/factory/Excel.java
+++ testSeek/src/main/java/au/com/seek/factory/Excel.java
@@ -1,7 +1,16 @@
 package au.com.seek.factory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Excel {
-	
-	
-	
+
+	//private static XSSFWorkbook ExcelWBook;
+
+	public static void openExcelFile(String path) throws FileNotFoundException {
+		FileInputStream ExcelFile = new FileInputStream(path);
+		//ExcelWBook = new XSSFWorkbook(ExcelFile);
+
+	}
+
 }
