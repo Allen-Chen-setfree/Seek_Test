810b5c9445977204b1ddda64e21b28888efbbb9b
unknown <allen.setfree.chen@gmail.com>
2018-02-27 13:58:01 +1100

    added test suite to inlcude more unit tests

---
+++ testSeek/src/test/java/au/com/seek/unitTests/Search.java
@@ -0,0 +1,5 @@
+package au.com.seek.unitTests;
+
+public class Search {
+
+}
---
+++ testSeek/src/test/java/au/com/seek/unitTests/UnitSuite.java
@@ -0,0 +1,15 @@
+package au.com.seek.unitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+	@RunWith(Suite.class)
+	@Suite.SuiteClasses({ 
+		SignIn.class,
+		Search.class
+
+	})
+
+public class UnitSuite {
+
+}
