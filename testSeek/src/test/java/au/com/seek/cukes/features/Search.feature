362b5f713eec0f7e46e963332ceb74ce065cafd9
unknown <allen.setfree.chen@gmail.com>
2018-02-28 22:10:49 +1100

    implemented SearchSteps except Date

--- testSeek/src/test/java/au/com/seek/cukes/features/Search.feature
+++ testSeek/src/test/java/au/com/seek/cukes/features/Search.feature
@@ -5,7 +5,7 @@ Scenario: Search "tester" in ICT field in Sydeny NSW for today and show the resu
 Given that the homepage "https://www.seek.com.au" is available
 When I input "tester" for "What"
 And tick the "Information & Communication Technology" in "Any Classification"
-And input "Sydeny" for "Where"
+And input "Sydney NSW" for "Where"
 And click "More options"
 And click "listed any time"
 And choose "Today"
--- testSeek/src/test/java/au/com/seek/cukes/steps/SearchSteps.java
+++ testSeek/src/test/java/au/com/seek/cukes/steps/SearchSteps.java
@@ -15,21 +15,21 @@ public class SearchSteps {
 	WebDriver driver;
 
 	@Given("^that the homepage \"([^\"]*)\" is available$")
-	public void that_the_homepage_is_available(String arg1) throws Throwable {
+	public void that_the_homepage_is_available(String URL) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
 		driver = DriverFactory.getBrowser("chrome");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-		driver.get(arg1);
+		driver.get(URL);
 
 		// throw new PendingException();
 	}
 
 	@When("^I input \"([^\"]*)\" for \"([^\"]*)\"$")
-	public void i_input_for(String arg1, String arg2) throws Throwable {
+	public void i_input_for(String keyword, String arg2) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// driver.findElement(By.id("SearchBar__Keywords")).sendKeys(arg1);
+		driver.findElement(By.id("SearchBar__Keywords")).sendKeys(keyword);
 
 		// throw new PendingException();
 	}
@@ -37,16 +37,17 @@ public class SearchSteps {
 	@When("^tick the \"([^\"]*)\" in \"([^\"]*)\"$")
 	public void tick_the_in(String arg1, String arg2) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// driver.findElement(By.className("1BytwqT v8b4xda")).click();
-		// driver.findElement(By.className("_3cvRF2q")).click();
+		driver.findElement(By.xpath("//*[@id=\"SearchBar\"]/div/div[1]/div/div[2]/label")).click();
+		driver.findElement(By.xpath("//*[@id=\"classificationsPanel\"]/nav/ul/li[18]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"SearchBar\"]/div/div[1]/div/div[2]/label")).click();
 
 		// throw new PendingException();
 	}
 
 	@When("^input \"([^\"]*)\" for \"([^\"]*)\"$")
-	public void input_for(String arg1, String arg2) throws Throwable {
+	public void input_for(String location, String arg2) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// driver.findElement(By.id("SearchBar__Where")).sendKeys(arg1);
+		driver.findElement(By.id("SearchBar__Where")).sendKeys(location);
 
 		// throw new PendingException();
 	}
@@ -54,22 +55,33 @@ public class SearchSteps {
 	@When("^click \"([^\"]*)\"$")
 	public void click(String arg1) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// driver.findElement(By.className("_117xZqt")).click();
-		// driver.findElement(By.className("_2jp6wVL _3qpSGs3")).click();
+
+		//driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div/div[1]/section/div[1]/div/div/div[2]/span"))
+		//		.click();
+		//driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div/div[1]/aside/div[1]/div/div/label[4]"))
+		//		.click();
+
 		// throw new PendingException();
 	}
 
 	@When("^choose \"([^\"]*)\"$")
 	public void choose(String arg1) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// driver.findElement(By.className("_1tBsnbd")).click();
-		// throw new PendingException();
+		// <a href="/jobs-in-information-communication-technology?daterange=1#"
+		// data-automation="1" role="radio" aria-checked="true" aria-label="Today"
+		// class="_1tBsnbd" target="_self" rel="nofollow">Today</a>
+		//driver.findElement(By.xpath("//*[@id=\"RefineDateListed__radiogroup\"]/li[2]/a")).click();
+		//driver.findElement(By.linkText("Today")).click();
+		//driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div/div[1]/aside/div[1]/div/div/label[4]"))
+		//.click();		
+		
+		//throw new PendingException();
 	}
 
 	@When("^I click \"([^\"]*)\"$")
 	public void i_click(String arg1) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// driver.findElement(By.xpath(".//*[@id='SearchBar']/button")).click();
+		driver.findElement(By.xpath(".//*[@id='SearchBar']/button")).click();
 		// throw new PendingException();
 
 	}
@@ -77,8 +89,8 @@ public class SearchSteps {
 	@Then("^I get the search results$")
 	public void i_get_the_search_results() throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		// System.out.print(driver.getTitle());
-		driver.quit();
+		System.out.print("the title of website is " + driver.getTitle());
+		// driver.close();
 		// throw new PendingException();
 	}
 
